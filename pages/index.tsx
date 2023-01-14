e7c73e601710c0e06fba35582b376ac1da18a2fd
bannzai <kingkong999yhirose@gmail.com>
2023-01-14 11:59:51 +0900

    Use ResizeObserver

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,40 @@
-import { Box, Container, Image, Stack, useDimensions } from "@chakra-ui/react";
-import { useEffect, useRef } from "react";
+import {
+  Box,
+  Container,
+  Heading,
+  Image,
+  List,
+  ListItem,
+  Stack,
+  useDimensions,
+} from "@chakra-ui/react";
+
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-  const elementRef = useRef(null);
-  const dimensions = useDimensions(elementRef, true);
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+  const boxRef = useRef(null);
 
   useEffect(() => {
-    console.log(dimensions);
-  }, [dimensions]);
+    const resizeObserver = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        setWidth(entry.contentRect.width);
+        setHeight(entry.contentRect.height);
+      }
+    });
+    if (boxRef.current != null) {
+      resizeObserver.observe(boxRef.current);
+    }
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, [boxRef]);
 
   return (
-    <Container
-      maxW="xl"
-      bg="blue.400"
-      centerContent
-      borderRadius="lg"
-      overflow="hidden"
-    >
-      <Stack padding="4" background={"white"} color="black" maxW="md">
-        <Image
-          src="https://avatars.githubusercontent.com/u/10897361?v=4"
-          alt="bannzai"
-        />
-
-        <Box
-          ref={elementRef}
-          resize="both"
-          width="100px"
-          height="100px"
-          borderWidth="1px"
-          borderRadius="lg"
-          overflow="auto"
-          borderColor="pink.400"
-          position={"absolute"}
-          left={"50%"}
-          top={"50%"}
-        />
-      </Stack>
-    </Container>
+    <Box ref={boxRef} resize={"both"} overflow={"auto"} bg="pink.400">
+      Box is {width}px wide and {height}px tall
+    </Box>
   );
 }
